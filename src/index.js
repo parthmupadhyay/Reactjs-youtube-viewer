65679787fc5c98d9028ce315876d80e2a494b64f
Parth Mukesh Upadhyay <parthmukesh.upadhyay@sjsu.edu>
2018-07-04 15:08:49 -0700

    full commit-v1

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import React ,{ Component } from 'react';
+import _ from 'lodash';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'put your api key here';
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = { 
+            videos:[],
+            selectedVideo:null 
+        };
+
+        this.videoSearch('ReactJS');
+    }
+
+    videoSearch(searchTerm){
+        YTSearch({key : API_KEY, term : searchTerm},(videos) => {
+            this.setState({
+                videos:videos,
+                selectedVideo:videos[0]
+            });
+        });
+    }
+
+    render(){
+
+        const videoSearch = _.debounce(searchTerm => this.videoSearch(searchTerm),300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange ={videoSearch}/>
+                <VideoDetail video= {this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}
+                />
+            </div>
+        );    
+    }
+    
+}
+
+ReactDOM.render(<App/>,document.querySelector('.container'));
